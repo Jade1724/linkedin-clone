6ebddbfb62539809fe6f255f57ab2cc18098952c
Jade1724 <haruka.i1724@gmail.com>
2022-07-30 02:25:34 +1200

    Created Join now and Sign in styled components.

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,10 @@ const Login = (props) => {
                 <a href="#">
                     <img src="/images/login-logo.svg" />
                 </a>
+                <div>
+                    <Join>Join now</Join>
+                    <SignIn>Sign in</SignIn>
+                </div>
             </Nav>
         </Container>
     )
@@ -15,7 +19,7 @@ const Login = (props) => {
 
 const Container = styled.div`
     padding: 0px;
-`
+`;
 
 const Nav = styled.nav`
     max-width: 1128px;
@@ -34,6 +38,39 @@ const Nav = styled.nav`
             padding: 0 5px;
         }
     }
-`
+`;
 
+const Join = styled.a`
+    font-size: 16px;
+    padding: 10px 12px;
+    text-decoration: none;
+    color: rgba(0, 0, 0, 0.6);
+    margin-right: 12px;
+    border-radius: 4px;
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.08);
+        color: rgba(0, 0, 0, 0.9);
+        text-decoration: none;
+    }
+`;
+
+const SignIn = styled.a`
+    box-shadow: inset 0 0 0 1px #0a66c2;
+    color: #0a66c2;
+    border-radius: 24px;
+    transition-duration: 167ms;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 40px;
+    padding: 10px 24px;
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0);
+
+    &:hover {
+        background-color: rgba(112, 181, 249, 0.15);
+        color: #0a66c2;
+        text-decoration: none;
+    }
+`;
 export default Login;
