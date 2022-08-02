20f631624c4d8842be1ec6451a7c2ea4bbde1710
Jade1724 <haruka.i1724@gmail.com>
2022-08-03 03:11:58 +1200

    Added a hiring in hurry section and grids with LeftSide, Main, RightSide component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -83,6 +83,7 @@ const Header = (props) => {
 const Container = styled.div`
     background-color: white;
     border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+    position: fixed;
     left: 0;
     padding: 0 24px;
     top: 0;
