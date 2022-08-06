08dcfa44de69e2b1b0c53aa6403484933e4c2513
Jade1724 <haruka.i1724@gmail.com>
2022-08-07 02:32:14 +1200

    Implemented sign out sign in redirection functionality.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,40 @@
 import { auth, provider } from "../firebase";
 import { signInWithPopup } from "firebase/auth";
+import { SET_USER } from "./actionType";
+
+export const setUser = (payload) => ({
+  type: SET_USER,
+  user: payload,
+});
 
 export function signInAPI() {
   return (dispatch) => {
     signInWithPopup(auth, provider).then((result) => {
-      console.log(result);
+      dispatch(setUser(result.user))
     }).catch((error) => {
         alert(error.message);
     })
     ;
   };
+}
+
+export function getUserAuth() {
+  return (dispatch) => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        dispatch(setUser(user));
+      }
+    })
+  }
+}
+
+export function signOutAPI() {
+  return (dispatch) => {
+    auth.signOut().then(() => {
+      dispatch(setUser(null));
+    })
+    .catch((error) => {
+      console.log(error.message);
+    })
+  }
 }
